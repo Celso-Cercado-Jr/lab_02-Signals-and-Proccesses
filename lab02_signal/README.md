c0ec71697906c3a558053b7b14f32b7d2c4aef9b
Celso Cercado Jr. <38006556+Celso-Cercado-Jr@users.noreply.github.com>
2018-09-19 11:27:44 -0400

    First

---
+++ lab02_signal/README.md
@@ -0,0 +1,7 @@
+# Title lab02_signal
+## Status
+My programs works and tested. It should be loop until the user will kill on neutrino.
+##Known Issues
+No, So far i discover of
+## Expected Grade
+Hopefully 4/5,decent I would say.
---
+++ lab02_signal/src/lab02_signal.c
@@ -0,0 +1,59 @@
+/*
+ ** sigint.c -- grabs SIGINT
+ *
+ * Source:  http://beej.us/guide/bgipc/html/single/bgipc.html#signals
+ *          http://beej.us/guide/bgipc/examples/sigint.c
+ *
+ * Modified by: hurdleg@algonquincollege.com
+ *
+ * Usage:
+ *  From Momentics IDE, run program; notice PID; enter some text, but don't hit the enter key
+ *  At Neutrino prompt, issue the command: kill -s SIGINT <PID>
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+volatile sig_atomic_t got_usr1;
+
+int usr1Happened = 1;
+
+void sigusr1_handler(int sig)
+{
+    got_usr1 = usr1Happened; // set usr1 to value of one
+
+}
+
+int main(void)
+{
+    struct sigaction sa;
+
+    got_usr1 = 0; //initialize the got
+
+    sa.sa_handler = sigusr1_handler;
+    sa.sa_flags = 0;
+
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("PID %d: Running...\n", getpid());
+    while (!got_usr1) {
+       // printf("PID %d: Running...\n", getpid());
+        printf("PID %d: Received USR1\n", getpid());
+       // sigusr1_handler(got_usr1);
+        sleep(1);
+    }
+
+    printf("Exiting!\n");
+
+    return 0;
+}
+
